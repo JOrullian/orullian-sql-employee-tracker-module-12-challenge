809af2d43db3984f16c088d76557b59d69e6a17c
JOrullian <jedediahorullian@gmail.com>
2024-08-23 08:45:59 -0500

    added comments to code

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const CLI = require('./lib/cli');
 const pool = require('./lib/db')
 
+// Create new instance of CLI class
 const cli = new CLI(pool);
 
+// Start by showing all employee data in a table, then run the app with inquirer prompts
 cli.showStartData()
   .then(() => {
     return cli.run();
